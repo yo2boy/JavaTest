82b7fc5d704dbc9dd0439b8f8aa444e3c37254f4
dunkleosteus <nikstick22@GMAIL.COM>
2014-11-20 17:18:17 -0500

    Removed DHT and enabled servers to receive parallel requests from clients and servers.
    TODO: setup multiple run configurations for servers 1, 2, 3, and 4

--- Course Project Server/src/Server.java
+++ Course Project Server/src/Server.java
@@ -10,9 +10,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Server 
+public class Server
 {
-	private ServerSocket serverSocket;
+
 	private int port;
 
 	public Server(int port) {
@@ -27,23 +27,63 @@ public class Server
 			System.out.println("DHT likely offline!");
 		}
 		System.out.println("Starting the socket server at port:" + port);
-		serverSocket = new ServerSocket(port);
-
-		//Listen for clients. Block till one connects
-
-		System.out.println("Waiting for clients...");
-		Socket client = serverSocket.accept();
-
-		//A client has connected to this server. Send welcome message
-		sendWelcomeMessage(client);
+		ServerThread thread = new ServerThread(port);
+		thread.start();
 	}
+
+	class ServerThread extends Thread
+	{
+		private ServerSocket serverSocket;
+		int port;
+		public ServerThread(int port) throws IOException{
+			this.port = port;
+			serverSocket = new ServerSocket(port);
+		}
+		public void run(){
+			//Listen for clients. Block till one connects
+
+			System.out.println("Waiting for clients...");
+
+			while(true){
+				Socket client;
+				try {
+					client = serverSocket.accept();
+					if(client != null){
+						SocketThread thread = new SocketThread(client);
+						thread.start();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class SocketThread extends Thread
+	{
+		Socket socket;
+		public SocketThread(Socket s){
+			this.socket = s;
+		}
+		public void run() {
+			try {
+				//A client has connected to this server. Send welcome message
+				sendWelcomeMessage(socket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 
 	public void connect() throws UnknownHostException, IOException{
 		String hostname = "localhost";
-		int DHTport = 9991;
-		System.out.println("Attempting to connect to "+hostname+":"+DHTport);
-		Socket DHTclient = new Socket(hostname,DHTport);
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(DHTclient.getOutputStream()));
+		int serverPort = 9991;
+		System.out.println("Attempting to connect to "+hostname+":"+serverPort);
+		Socket serverClient = new Socket(hostname, serverPort);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(serverClient.getOutputStream()));
 		writer.write(getIP());
 		writer.flush();
 		writer.close();
@@ -76,17 +116,24 @@ public class Server
 	}
 
 	public static void main(String[] args) {
-		// Setting a default port number.
-		int portNumber = 9990;
+		// Setting a default port number for clients.
+		int portNumber_C = 9990;
+		// Setting a default port number for servers in the DHT.
+		int portNumber_S = 9991;
 
 		try {
-			// initializing the Socket Server
-			Server socketServer = new Server(portNumber);
+			// initializing the Socket Server for the client
+			Server socketServer_C = new Server(portNumber_C);
 
-			socketServer.start();
+			// initializing the Socket Server for the server
+			Server socketServer_S = new Server(portNumber_S);
+
+			socketServer_S.start();
+			socketServer_C.start();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 }
